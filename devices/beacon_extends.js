49616f3894ecfae7a6f759b5ed80e78aac48d1d1
stanlee321 <stanlee321@gmail.com>
2019-02-27 00:45:18 -0400

    fixes

--- devices/beacon_extends.js
+++ devices/beacon_extends.js
@@ -4,7 +4,13 @@ const BeaconListener = require('./beacon_base.js')
 module.exports = class BeaconModule extends BeaconListener{
     status(){
         console.log('ok')
-    }
+    };
+    
+    insertData(my_data){
+        console.log("Insert data")
+        this.db.run('INSERT INTO beacon (time, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', my_data );
+      }
+      
 }
 
   
