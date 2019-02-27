1ea1080b56c8da8bea31185790e096aa75e64e75
stanlee321 <stanlee321@gmail.com>
2019-02-27 00:37:15 -0400

    updated db fixes

--- db/db.js
+++ db/db.js
@@ -16,14 +16,12 @@ module.exports = class DataBase {
     }
 
     createTable(){
-        this.db.run("CREATE TABLE IF NOT EXISTS beacon(id INTEGER PRIMARY KEY AUTOINCREMENT, time STRING, _0 INTEGER, _1 INTEGER, _2 INTEGER, _3 INTEGER, _4 INTEGER, _5 INTEGER, _6 INTEGER, _7 INTEGER, _8 INTEGER, _9 INTEGER, _10 INTEGER, _11 INTEGER, _12 INTEGER, _13 INTEGER, _14 INTEGER, _15 INTEGER, _16 INTEGER, _17 INTEGER, _18 INTEGER, _19 INTEGER, _20 INTEGER, _21 INTEGER)",
-                insertData);
+        this.db.run("CREATE TABLE IF NOT EXISTS beacon(id INTEGER PRIMARY KEY AUTOINCREMENT, time STRING, _0 INTEGER, _1 INTEGER, _2 INTEGER, _3 INTEGER, _4 INTEGER, _5 INTEGER, _6 INTEGER, _7 INTEGER, _8 INTEGER, _9 INTEGER, _10 INTEGER, _11 INTEGER, _12 INTEGER, _13 INTEGER, _14 INTEGER, _15 INTEGER, _16 INTEGER, _17 INTEGER, _18 INTEGER, _19 INTEGER, _20 INTEGER, _21 INTEGER)",insertData);
     }
 
     insertData(my_data){
         console.log("Insert data")
-        this.db.run('INSERT INTO beacon (time, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', 
-                my_data );
+        this.db.run('INSERT INTO beacon (time, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', my_data );
       }
       
 
